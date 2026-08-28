aead49c5c05dac8e2aae1aa67cdaf0e2bfbc4872
jazir555 <techtutormm@gmail.com>
2026-08-28 16:01:08 -0700

    up

--- XiZerosInfiniteScratch.lean
+++ XiZerosInfiniteScratch.lean
@@ -1,5 +1,7 @@
 import ZeroFreeRegionHadamard
-import Zeta23.Final
 import Zeta23.Assembly
+import Zeta23.RvM.Statement
+import Zeta23.Statement.SeamClosed
+import Zeta23.GammaFacts.Complete
 
 example : True := trivial
